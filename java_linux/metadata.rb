32feeff14700f3723869bc4dc828aa59ae1029d1
Kyle Kelley <kyle.kelley@rackspace.com>
2013-11-08 17:31:25 -0600

    Grab jclouds, install maven

--- java_linux/metadata.rb
+++ java_linux/metadata.rb
@@ -2,8 +2,10 @@ name             'java_linux'
 maintainer       'Kyle Kelley'
 maintainer_email 'kyle.kelley@rackspace.com'
 license          'Apache 2.0'
-description      'Installs/Configures java_linux'
+description      'Installs/Configures java on a linux box'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
 depends "apt", "~> 2.0.0"
+depends "java", "~> 1.15.4"
+depends "maven", "~> 1.0.1"
--- java_linux/recipes/default.rb
+++ java_linux/recipes/default.rb
@@ -6,3 +6,25 @@
 # 
 # All rights reserved - Do Not Redistribute
 #
+
+# Note, this is fast and loose get jclouds up with the examples on a vagrant box (abusing defaults)
+
+package "git" do
+  action :install
+end
+
+# https://github.com/jclouds/jclouds-examples/tree/master/rackspace
+
+git "/home/vagrant/jclouds-examples" do
+  repository 'https://github.com/jclouds/jclouds-examples.git'
+  reference 'master'
+  action :sync
+end
+
+bash "Get dependencies for jclouds examples" do
+  user "vagrant"
+  cwd "/home/vagrant/jclouds-examples/rackspace"
+  code <<-EOH
+  mvn dependency:copy-dependencies "-DoutputDirectory=./lib" "-Dclassifier=sources"
+  EOH
+end
