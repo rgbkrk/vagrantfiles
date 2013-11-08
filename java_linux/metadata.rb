3ac9654ad98cf330a664512528bc20ce6d0f64f5
Kyle Kelley <kyle.kelley@rackspace.com>
2013-11-08 15:21:26 -0600

    Add apt

--- java_linux/metadata.rb
+++ java_linux/metadata.rb
@@ -1,8 +1,9 @@
 name             'java_linux'
-maintainer       'YOUR_NAME'
-maintainer_email 'YOUR_EMAIL'
-license          'All rights reserved'
+maintainer       'Kyle Kelley'
+maintainer_email 'kyle.kelley@rackspace.com'
+license          'Apache 2.0'
 description      'Installs/Configures java_linux'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
+depends "apt", "~> 2.0.0"
